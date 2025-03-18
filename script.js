0eb1483b041333ab9b9c12efcaff92cfd4bc7f5e
Esteban <glo3025pando@gmail.com>
2025-03-18 17:43:04 -0300

    Modificacion de toda la pagina

--- script.js
+++ script.js
@@ -1,96 +1,21 @@
-const btn = document.getElementById('btn');
-const codeInput = document.getElementById('code-input');
-const resultText = document.getElementById('result-text');
-const deleteBtn = document.getElementById('delete-btn');
-const result = document.getElementById('result');
-const fecha = document.getElementById('date');
-const fileInput = document.getElementById('file');
-const imgPreview = document.getElementById('img-preview');
-const reader = new FileReader();
-let count = 0;
-const date = new Date();
-const day = date.getDate();
-const month = date.getMonth() + 1;
-const year = date.getFullYear();
-const today = `${day}/${month}/${year}`;
-fecha.innerText += " " + today;
+const textInput = document.getElementById('text-input');
+const makeBtn = document.getElementById('make-btn');
+const downloadBtn = document.getElementById('download-btn');
+const resetBtn = document.getElementById('reset-btn');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
+function draw() {
+    const ctx = canvas.getContext("2d");
+    canvas.width = 350;
+    const x = canvas.width / 2;
 
-const printResult = () => {
-    if (codeInput.value === '') {
-        alert('Ingrese un código');
-        count = 0;
-        return;
-    }
-    count++;
-    if (count > 1) {
-        alert('Solo se puede enviar un código');
-        codeInput.value = '';
-        return;
-    }
-    const code = codeInput.value;
-    const cleanCode = cleanInputString(code);
-    const codigos = [];
-    
-    for (let i = 0; i < cleanCode.length; i+= 5) {
-        if (cleanCode.length < 5) {
-            window.alert('No se puede enviar un código menor a 5 dígitos');
-            deleteResult();
-            return;
-        }
-        codigos.push(cleanCode.slice(i, i + 5));
-    };
-
-    resultText.innerText = codigos.join(' ');
-    
-    for (let i = 0; i < codigos.length; i++) {
-        console.log(codigos[i]);
-    }
-    codeInput.value = '';
+    ctx.font = "1rem Times New Roman";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("Hola mundo", x, 30);
 }
 
-function cleanInputString(str) {
-    const regex = /[+-\s]/g;
-    return str.replace(regex, '');
-}
-
-const deleteResult = () => {
-    codeInput.value = '';
-    resultText.innerText = '';
-    count = 0; 
-}
-
-codeInput.addEventListener("keydown", (e) => {
-    if (e.key === 'Enter') {
-        printResult();
-    };
-});
+draw();
 
 
-function handleEvent(event) {
-    if (event.type === "load"){
-        imgPreview.src = reader.result;
-    };
-}
-
-function addListeners(reader) {
-    reader.addEventListener("loadstart", handleEvent);
-    reader.addEventListener("load", handleEvent);
-    reader.addEventListener("loadend", handleEvent);
-    reader.addEventListener("progress", handleEvent);
-    reader.addEventListener("error", handleEvent);
-    reader.addEventListener("abort", handleEvent);
-}
-
-function handleSelected(e) {
-    eventLog.textContent = "";
-    const selectedFile = fileInput.files[0];
-    if (selectedFile) {
-      addListeners(reader);
-      reader.readAsDataURL(selectedFile);
-    }
-}
-
-file.addEventListener("change", handleSelected);
-btn.addEventListener('click', printResult);
-deleteBtn.addEventListener('click', deleteResult);
